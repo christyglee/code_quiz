9cc7a114acc92a671364ecebc28ce821c8d2b3cc
Christy Lee <christy.g.lee@gmail.com>
2020-09-22 21:51:19 -0700

    established variables in script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Adding variables
+
+var startButton = document.getElementById("btn");
+var quizEl = document.getElementById("quiz");
+var rulesEl = document.getElementById("card-text");
+var answerEl = document.getElementById("answer");
+
+
+
