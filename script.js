ad41e4c1061ca3833b98178c2ee9af9c62b31dc0
Christy Lee <christy.g.lee@gmail.com>
2020-09-23 19:12:49 -0700

    input question 1 in html and addEventListener in js

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ var startButton = document.getElementById("btn");
 var quizEl = document.getElementById("quiz");
 var rulesEl = document.getElementById("card-text");
 var answerEl = document.getElementById("answer");
+var timer;
 
 
-
+// Add Event Listener
+startButton.addEventListener("click", function() {
+    rulesEl.style.display = "none";
+    startButton.style.display = "none";
+    quizEl.style.display = "block";
+    quizTimer();
+});
